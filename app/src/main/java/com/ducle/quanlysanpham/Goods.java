e0070247632277c741dbbaa3b78cc1e8a2701607
benbeckman <levuanhduc1996@gmail.com>
2019-04-12 18:50:58 +0700

    tuan6-bai4-quanLySanPham

---
+++ app/src/main/java/com/ducle/quanlysanpham/Goods.java
@@ -0,0 +1,29 @@
+package com.ducle.quanlysanpham;
+
+public class Goods {
+    private String id;
+    private String name;
+    public String getid() {
+        return id;
+    }
+    public void setid(String id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public Goods(String id, String name) {
+        super();
+        this.id = id;
+        this.name = name;
+    }
+    public Goods() {
+        super();
+    }
+    public String toString() {
+        return this.id+" - "+this.name;
+    }
+}
